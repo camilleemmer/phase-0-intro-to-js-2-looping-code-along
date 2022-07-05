24475e2065ac9a43fe050dd9e43dca82e219d413
Camille Emmer <camilleemmer@gmail.com>
2022-07-05 12:23:21 -0600

    commit tree fe2ac64d813a02346de52a68dce1287a69f12aa4

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solutions in this file
+// for(let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);   
+// }
+
+// const gifts = ['teddy bear', 'drone', 'doll'];
+// function wrapGifts(gifts) {
+//     for(let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+const note = [];
+const event = [];
+function writeCards(note, event) {
+    let newArray = []
+    for(let i = 0; i < note.length; i++){
+       newArray.push(`Thank you, ${note[i]}, for the wonderful surprise gift!`);
+    }
+return newArray;
+    }
+
+// function countDown(integer);
+// let countDown = 10;
+// while (countDown <= 10) {
+//   console.log(countDown--);
+  
+// }
+
+function countDown(int){
+  let i =0;
+  while (i<11) {
+    console.log(i)
+    i++;
+  }
+  return int;
+}
